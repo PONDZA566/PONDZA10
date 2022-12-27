87706f7339d087fa2ade159cda20542c309af2d2
PONDZA <thanat_t@cmu.ac.th>
2022-12-27 16:40:54 +0700

    lab10

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int M,int N){
+	if(M<=0 || N<=0){
+		cout << "Invalid input";
+	}
+	for(int i = 0;i < M; i++){
+		for(int j = 0 ; j < N ;j++){
+			cout << 'O';
+		}
+		cout << '\n';
+	}
+}
 
 int main(){
 	
